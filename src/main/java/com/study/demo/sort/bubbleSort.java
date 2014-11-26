0e0a32ea0b617b661fd3e2af4e3cbe4f0adfaefd
adley-yang <adleyyang.cn@gmail.com>
2014-11-26 19:00:08 +0800

    Initial commit

---
+++ src/main/java/com/study/demo/sort/bubbleSort.java
@@ -0,0 +1,86 @@
+package com.study.demo.sort;
+
+/**
+ * Project Name:Arithmetic
+ * Package Name:com.study.demo.sort
+ * Date:14-11-26 下午6:35
+ * Copyright (c) 2014, adleyyang.cn@gmail.com All Rights Reserved.
+ */
+class ArrayBub
+{
+    private long[] a;                 // ref to array a
+    private int nElems;               // number of data items
+    //--------------------------------------------------------------
+    public ArrayBub(int max)          // constructor
+    {
+        a = new long[max];                 // create the array
+        nElems = 0;                        // no items yet
+    }
+    //--------------------------------------------------------------
+    public void insert(long value)    // put element into array
+    {
+        a[nElems] = value;             // insert it
+        nElems++;                      // increment size
+    }
+    //--------------------------------------------------------------
+    public void display()             // displays array contents
+    {
+        for(int j=0; j<nElems; j++)       // for each element,
+            System.out.print(a[j] + " ");  // display it
+        System.out.println("");
+    }
+    //--------------------------------------------------------------
+
+    /**
+     * 冒泡算法：（稳定排序算法）
+     * 1.比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+     * 2.对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
+     * 3.针对所有的元素重复以上的步骤，除了最后一个。
+     * 4.持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+     *
+     */
+    public void bubbleSort()
+    {
+        int out, in;
+
+        for(out=nElems-1; out>1; out--)   // outer loop (backward)
+            for(in=0; in<out; in++)        // inner loop (forward)
+                if( a[in] > a[in+1] )       // out of order?
+                    swap(in, in+1);          // swap them
+    }  // end bubbleSort()
+    //--------------------------------------------------------------
+    private void swap(int one, int two)
+    {
+        long temp = a[one];
+        a[one] = a[two];
+        a[two] = temp;
+    }
+//--------------------------------------------------------------
+}  // end class ArrayBub
+////////////////////////////////////////////////////////////////
+class BubbleSortApp
+{
+    public static void main(String[] args)
+    {
+        int maxSize = 100;            // array size
+        ArrayBub arr;                 // reference to array
+        arr = new ArrayBub(maxSize);  // create the array
+
+        arr.insert(77);               // insert 10 items
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+
+        arr.display();                // display items
+
+        arr.bubbleSort();             // bubble sort them
+
+        arr.display();                // display them again
+    }  // end main()
+}  // end class BubbleSortApp
